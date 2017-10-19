c8a3eea03c3beb641c3be6bb8f4f537549861417
deep-Turquoise <nicholasnelson10@gmail.com>
2017-10-19 12:02:26 -0700

    more queries to come that i'm using on the webpage

--- CST363_Final_Queries.sql
+++ CST363_Final_Queries.sql
@@ -5,3 +5,15 @@
 SELECT cst336final.Movies.Ttitle, cst336final.Movies.Genre, cst336final.MovieStats.AvgUserRating 
 FROM cst336final.Movies JOIN cst336final.MovieStats
 ON cst336final.Movies.MovieId = cst336final.MovieStats.MovieId;
+
+
+-- -----------------------------------------------------
+-- Selects from Movies table to display just the Genre
+-- -----------------------------------------------------
+SELECT DISTINCT(Genre) FROM Movies
+
+
+-- -----------------------------------------------------
+-- Selects from Movies table to display just the title and Year as: Title (Year)
+-- -----------------------------------------------------
+SELECT CONCAT(title, ' (', Year, ' )') AS selection FROM Movies
