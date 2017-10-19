23ea0a714c7ccb4c6f4c55bd9382381e4ca9ec42
deep-Turquoise <nicholasnelson10@gmail.com>
2017-10-19 14:21:24 -0700

    cleaned up queries i made for the site

--- CST363_Final_Queries.sql
+++ CST363_Final_Queries.sql
@@ -8,20 +8,36 @@ ON cst336final.Movies.MovieId = cst336final.MovieStats.MovieId;
 
 
 -- -----------------------------------------------------
--- Selects from Movies table to display just the Genre
+-- Selects each avaible Genre from Movies once
 -- -----------------------------------------------------
 SELECT DISTINCT(Genre) FROM Movies
 
 
 -- -----------------------------------------------------
--- Selects from Movies table to display just the title and Year as: Title (Year)
+-- Selects and Concats title and year as selection from Movies. Output will look like: "movie_name (yyyy)"
 -- -----------------------------------------------------
-SELECT CONCAT(title, ' (', Year, ' )') AS selection FROM Movies
+SELECT CONCAT(title, ' (', Year, ')') AS selection FROM Movies
 
 
 -- -----------------------------------------------------
--- Uses Inner Joins to collect similar data based on tags to output complete movie information
+-- Selects from Movie, Director, Movie and Movie Stats. Uses 4 INNER JOIN statements to insure MovieId is 
+-- similar across all. INNER JOIN on CastCrew acts as a conduit for MovieId, DirectorId and ActorId
 -- -----------------------------------------------------
+ SELECT m.Title AS MOVIE_NAME, m.Year AS MOVIE_YEAR, m.Genre AS MOVIE_GENRE,
+                ms.Budget AS MOVIE_BUDGET, ms.Gross AS MOVIE_GROSS, ms.AcademyAwards AS MOVIE_ACADEMYAWARDS, ms.AvgUserRating AS MOVIE_AVGUSERRATING,
+                CONCAT(d.Firstname, ' ', d.LastName) AS MOVIE_DIRECTORNAME,
+                CONCAT(a.FirstName,' ', a.LastName) AS MOVIE_ACTORNAME, a.AcademyAwardWinner AS MOVIE_ACADEMYAWARDWINNER
+ FROM Movies m
+ INNER JOIN MovieStats ms ON m.MovieId = ms.MovieId
+ INNER JOIN CastCrew cc ON ms.MovieId = cc.MovieId
+ INNER JOIN Director d ON d.DirectorId = cc.Director
+ INNER JOIN Actors a ON a.ActorId = cc.LeadActor
+
+
+-- -----------------------------------------------------
+-- Creates a View of the previous SELECT statement
+-- -----------------------------------------------------
+ CREATE VIEW Complete_Information AS
  SELECT m.Title AS MOVIE_NAME, m.Year AS MOVIE_YEAR, m.Genre AS MOVIE_GENRE,
                 ms.Budget AS MOVIE_BUDGET, ms.Gross AS MOVIE_GROSS, ms.AcademyAwards AS MOVIE_ACADEMYAWARDS, ms.AvgUserRating AS MOVIE_AVGUSERRATING,
                 CONCAT(d.Firstname, ' ', d.LastName) AS MOVIE_DIRECTORNAME,
